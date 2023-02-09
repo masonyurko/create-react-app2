b458e26213ab29fa0137798325871a17a2d7a813
masonyurko <122504955+masonyurko@users.noreply.github.com>
2023-02-08 23:25:57 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+
+<>
+    <div class="outline">
+      <div class="card">
+        <h2>Jalen Hurts</h2>
+        <img src="https://static.clubs.nfl.com/image/private/t_editorial_landscape_6_desktop_2x/f_auto/v1628455300/eagles/zuftq3b6azlzvkoznb2v.jpg" alt="Jalen Hurts" />
+        <p class="details">Philadelphia Eagles <br></br> Quarterback </p>
+      </div>
+    </div>
+  <div class="buttons">
+  
+  <button class="copy">Copy</button>
+  <button id="del">Delete</button>
+  <button class="btnDet">Toggle Details</button> 
+  <button class="btnCT">Change Title</button>
+  <button  class="btnBG">Change Background Color</button>
+  
+  </div>
+  
+</>
+  );
+}
+
+export default App;
